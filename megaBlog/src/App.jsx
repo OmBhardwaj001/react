0c0edef62bea65ef502fd8bce8faa8580b7ced6c
om bhardwaj <ombhardwaj155@gmail.com>
2025-09-17 23:48:19 +0530

    file structure setup

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,9 +1,40 @@
+import React,{useState,useEffect} from "react";
+import {useDispatch} from 'react-redux';
+import authService from './appwrite/auth';
+import { login,logout } from "./store/authSlice";
+import {Header,Footer} from './components/index'
+
 function App() {
-  return(
-    <>
-      <h1> hello world </h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService .getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{
+      console.log("error fetching current user:",error);
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="min-h-sc  flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 
 
   
